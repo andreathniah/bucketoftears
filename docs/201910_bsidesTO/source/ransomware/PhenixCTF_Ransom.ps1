c960d7a084e5233acd2ed36c312df6f32aedf7be
root <root@localhost.localdomain>
2019-10-07 20:27:09 -0400

    add source files of bsidesTO challenges

---
+++ docs/201910_bsidesTO/source/ransomware/PhenixCTF_Ransom.ps1
@@ -0,0 +1,19 @@
+﻿function GetTheFlagOrNot
+{
+    Param(
+        [Parameter(Mandatory = $true, Position = 0)]
+        [int]$EncryptionKey
+    )
+
+    Get-ChildItem .\flag.txt | % { 
+        $EncryptedData = "";
+        (Get-Content -Encoding ASCII $_.FullName).ToCharArray() | % {
+            $EncryptedData += [char]($_ -bxor $EncryptionKey)
+   
+        }
+        Set-Content -Path $_.FullName -Value $EncryptedData
+        Write-Output "All Your Files Are Belong To Me Now!"
+    }
+}
+
+
